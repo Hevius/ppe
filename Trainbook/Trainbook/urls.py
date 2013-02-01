b3af306fcaeec6c037c0d911228c15cf70ef2ad6
legum <paul.chabane@gmail.com>
2013-02-01 11:08:35 +0100

    Ceci est un test J'ai enlevé le commentaire dans urls.py

--- Trainbook/Trainbook/urls.py
+++ Trainbook/Trainbook/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'Trainbook.views.home', name='home'),
-    # url(r'^Trainbook/', include('Trainbook.Trainbook.urls')),
+    url(r'^Trainbook/', include('Trainbook.Trainbook.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
