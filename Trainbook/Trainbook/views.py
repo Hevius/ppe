431fb2f830ead87c8908278168416c015539c908
Hevius <p.p.m.alexis@gmail.com>
2013-02-01 15:09:35 +0100

    Hello World !

--- Trainbook/Trainbook/views.py
+++ Trainbook/Trainbook/views.py
@@ -3,5 +3,5 @@
 from django.shortcuts import render_to_response
 from datetime import datetime
 
-def welcome(request):
-    return render_to_response ('base.html')
+def home(request):
+    return render_to_response('index.html')
