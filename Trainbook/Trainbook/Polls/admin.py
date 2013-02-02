bf1b7013d5939256dbeedb9d785e4054e0a541cb
Hevius <p.p.m.alexis@gmail.com>
2013-02-02 13:45:05 +0100

    Add admin + others features

---
+++ Trainbook/Trainbook/Polls/admin.py
@@ -0,0 +1,26 @@
+from Trainbook.Polls.models import Poll
+from Trainbook.Polls.models import Choice
+from django.contrib import admin
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ('question', 'pub_date', 'was_published_today')
+    list_filter = ['pub_date']
+    search_fields = ['question']
+    date_hierarchy = 'pub_date'
+
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+    def was_published_today(self):
+        return self.pub_date.date() == datetime.date.today()
+
+    was_published_today.short_description = u'Publi&#232; aujourd\'hui ?'
+
+admin.site.register(Poll, PollAdmin)
--- Trainbook/Trainbook/settings.py
+++ Trainbook/Trainbook/settings.py
@@ -15,7 +15,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'trainbook',                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(PROJECT_ROOT,'trainbook'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -124,9 +124,10 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'Trainbook.Polls',
 )
 
 # A sample logging configuration. The only tangible logging
